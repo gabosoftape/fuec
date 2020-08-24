8a16073aef597e90d0e52bbcf4fb3bab0006e240
Gab <gabosoft>
2020-08-24 16:54:54 -0500

    update model

--- models/wialon_user.py
+++ models/wialon_user.py
@@ -7,13 +7,6 @@ class wialon_pseudouser(models.Model):
     firma = fields.Binary('Firma')
     logo = fields.Binary('Logo')
     razonsocial = fields.Char("Razon social")
-    website = fields.Char("Sitio Web")
-    nit = fields.Char("NIT")
-    fuec_code_hab = fields.Char("Codigo de habilitacion temporal")
-    telefono = fields.Char("Telefono")
-    fuec_resolucion = fields.Char("No. de resolucion")
-    email = fields.Char("Email")
-    fuec_fecha_hab = fields.Date("Fecha de habilitacion")
     id_type = fields.Selection(
         string=u'Tipo de Documento',
         selection=[
@@ -31,3 +24,10 @@ class wialon_pseudouser(models.Model):
         help=u'Identificacion del Cliente',
     )
     id_document = fields.Integer(string='No. Documento', default=None)
+    celular = fields.Char("Celular")
+    email = fields.Char("Email")
+    website = fields.Char("Sitio Web")
+    direccion = fields.Char('Direccion')
+    fuec_code_hab = fields.Char("Codigo de habilitacion temporal")
+    fuec_resolucion = fields.Char("No. de resolucion")
+    fuec_fecha_hab = fields.Date("Fecha de habilitacion")
