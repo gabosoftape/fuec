451b93184dc4a0eaf6487bf1f354e0859ba4ec32
gabriel <gabosoft.ape@outlook.com>
2020-09-22 10:34:50 -0500

    update wizard back call pdf fuec

--- wizard/fuec_report_wizard.py
+++ wizard/fuec_report_wizard.py
@@ -1,6 +1,9 @@
+import base64
+
 import qrcode
 
 from odoo import models, fields, api, _
+from io import BytesIO
 
 
 class FuecReportButton(models.TransientModel):
@@ -16,59 +19,87 @@ class FuecReportButton(models.TransientModel):
             folio = self.fuec_select.folio
             fuec = self.env['gpscontrol.fuec'].search([('folio', '=', folio)])
         user_id = fuec.create_uid
-        pseudouser = self.env['gpscontrol.wialon_pseudouser'].search(
-            [('user_id', '=', user_id.id)])
-        folio = fuec.folio
-        contract_id = fuec.contract_id
+        pseudouser = self.env['gpscontrol.wialon_pseudouser'].sudo().search(
+            [('user_id', '=', self.env.user.id)])
+        array_conductores = []
+        array_responsables = []
+        my_fuec = fuec
+        folio = my_fuec.folio
+        contract_id = my_fuec.contract_id
         razonsocial = pseudouser.razonsocial
+        direccion = pseudouser.direccion
+        telefono = pseudouser.celular
+        email = pseudouser.email
         id_type = pseudouser.id_type
         id_document = pseudouser.id_document
-        contratista_id = fuec.contratista
-        contratista = self.env['gpscontrol.contratista_fuec'].search([('id', '=', int(contratista_id))])
-        responsable_id = fuec.responsables
-        responsables = self.env['gpscontrol.responsable_fuec'].search([('id', 'in', responsable_id.ids)])
-        ruta_id = fuec.ruta
-        ruta = self.env['gpscontrol.ruta_fuec'].search([('id', '=', int(ruta_id))])
-        # REMOVER: descripcion_recorrido = fuec.descripcion
-        convenio = fuec.convenio
-        ut_con = fuec.ut_externa
-        inicio_contrato = fuec.fecha_ini
-        fin_contrato = fuec.fecha_end
-        vehiculo = fuec.vehiculo
-        conductores = fuec.conductores
+        contratista = my_fuec.contratista
+        ruta = my_fuec.ruta
+        convenio = my_fuec.convenio
+        ut_con = my_fuec.ut_externa
+        inicio_contrato = my_fuec.fecha_ini
+        fin_contrato = my_fuec.fecha_end
+        vehi_placa = my_fuec.vehiculo.registration_plate
+        vehi_modelo = my_fuec.vehiculo.year
+        vehi_marca = my_fuec.vehiculo.brand
+        vehi_clase = my_fuec.vehiculo.model
+        vehi_interno = my_fuec.vehiculo.vin
+        vehi_operacion = my_fuec.vehiculo.noperacion
+        date_operacion = my_fuec.vehiculo.voperacion
+        logo = pseudouser.logo
+        for responsible in my_fuec.responsables:
+            array_responsables.append(responsible.id)
+        # get data of responsables
+        responses = self.env['gpscontrol.responsable_fuec'].sudo().search([('id', 'in', array_responsables
+                                                                               )])
+        for conductor in my_fuec.conductores:
+            array_conductores.append([conductor.name, conductor.cedula,
+                                      conductor.licencia, conductor.vencimiento])
+        pdf_link = 'https://monitoringinnovation.com/fuec/my/{}?report_type=pdf'.format(fuec.id)
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
             box_size=10,
             border=4,
         )
-        qr.add_data('Some data')
+        qr.add_data(pdf_link)
         qr.make(fit=True)
-
-        img = qr.make_image(fill_color="black", back_color="white")
+        img = qr.make_image()
+        temp = BytesIO()
+        img.save(temp, format="PNG")
+        qr_image = base64.b64encode(temp.getvalue())
         firma = pseudouser.firma
         data = {
             'report_type': 'pdf',
             'folio': folio,
+            'logo': logo,
             'razonsocial': razonsocial,
+            'direccion': direccion,
+            'telefono': telefono,
+            'email': email,
             'contract_id': contract_id,
             'id_type': id_type,
             'id_document': id_document,
+            'contratista_empresa': contratista.empresa,
             'contratista_nombre': contratista.nombre,
             'contratista_nit': contratista.nit,
-            'responsables': responsables,
+            'responsibly': responses,
             'ruta_nombre': ruta.nombre,
             'ruta_det': ruta.detalle,
             'convenio': convenio,
             'ut_con': ut_con,
             'inicio_contrato': inicio_contrato,
             'fin_contrato': fin_contrato,
-            'vehiculo': vehiculo,
-            'conductores': conductores,
-            'qr': img,
+            'vehi_placa': vehi_placa,
+            'vehi_modelo': vehi_modelo,
+            'vehi_marca': vehi_marca,
+            'vehi_clase': vehi_clase,
+            'vehi_interno': vehi_interno,
+            'vehi_operacion': vehi_operacion,
+            'conductores': array_conductores,
+            'qr': qr_image,
             'pseudouser': pseudouser,
             'firma': firma,
-            'contract_object': fuec.contract_object,
+            'contract_object': my_fuec.contract_object,
         }
         #Datos
         #print(data)
